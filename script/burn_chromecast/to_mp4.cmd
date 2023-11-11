bcfd21ee8b087ec48cf42dbf1b612713a0f57573
Alexey Tokar <tokar.a.v@yandex.ru>
2023-11-11 19:52:14 +0300

    to_mp4 script

---
+++ script/burn_chromecast/to_mp4.cmd
@@ -0,0 +1,16 @@
+@echo on
+
+REM /b - plain out, /s - full paths /a-d files only /on sorted alpha asc
+md mp4
+for /f "tokens=*" %%a in ('
+    dir /b
+    /s
+    /a-d
+    /on
+    *.mkv
+') do (
+    @echo "in: %%a out: mp4/%%~na subs: %%~na%%~xa"
+    c:\soft\ffmpeg\bin\ffmpeg.exe -i "%%a" ^
+        -vf subtitles="%%~na%%~xa":si=2 ^
+        -map 0:0 -map 0:2 "mp4/%%~na.mp4"
+)
