55dc5201fe7686c32b345415a565aafd14523e98
Alexey Tokar <tokar.a.v@yandex.ru>
2023-11-11 20:46:02 +0300

    add h264 profile

--- script/burn_chromecast/to_mp4.cmd
+++ script/burn_chromecast/to_mp4.cmd
@@ -12,5 +12,6 @@ for /f "tokens=*" %%a in ('
     @echo "in: %%a out: mp4/%%~na subs: %%~na%%~xa"
     c:\soft\ffmpeg\bin\ffmpeg.exe -i "%%a" ^
         -vf subtitles="%%~na%%~xa":si=2 ^
+        -profile:v baseline -level:v 4.1 ^
         -map 0:0 -map 0:2 "mp4/%%~na.mp4"
 )
