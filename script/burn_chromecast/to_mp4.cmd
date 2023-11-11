702952c807b7a7731930eb419c6f27b2a2e380a1
Alexey Tokar <tokar.a.v@yandex.ru>
2023-11-11 22:03:50 +0300

    add strip metadata for good

--- script/burn_chromecast/to_mp4.cmd
+++ script/burn_chromecast/to_mp4.cmd
@@ -12,6 +12,10 @@ for /f "tokens=*" %%a in ('
     @echo "in: %%a out: mp4/%%~na subs: %%~na%%~xa"
     c:\soft\ffmpeg\bin\ffmpeg.exe -i "%%a" ^
         -vf subtitles="%%~na%%~xa":si=2 ^
-        -profile:v baseline -level:v 4.1 ^
-        -map 0:0 -map 0:2 "mp4/%%~na.mp4"
+        -preset fast -c:a aac -b:a 192k -ac 2 ^
+        -c:v libx264 ^
+        -profile:v high -level:v 4.1 ^
+        -pix_fmt yuv420p ^
+        -map_metadata -1 ^
+        -map 0:0 -map 0:2 "../dlna/bb1/%%~na.mp4"
 )
